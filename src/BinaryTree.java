67042a1ba71b91a53d4706a32431c471ec37522b
Maquito365 <macsoloj@gmail.com>
2026-03-20 17:01:12 -0600

    Se implemento la estructura base del Binary Tree, association representa pares clave-valor (ingles-español) TreeNode nodos del arbol y desarrollo de BinaryTree con insert y search

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,4 +1,54 @@
 package src;
-public class BinaryTree {
-    
+public class BinaryTree<E extends Comparable<E>> {
+
+    private TreeNode<E> root;
+
+    // INSERT
+    public void insert(E value) {
+        root = insertRec(root, value);
+    }
+
+    private TreeNode<E> insertRec(TreeNode<E> node, E value) {
+        if (node == null) {
+            return new TreeNode<>(value);
+        }
+
+        if (value.compareTo(node.data) < 0) {
+            node.left = insertRec(node.left, value);
+        } else {
+            node.right = insertRec(node.right, value);
+        }
+
+        return node;
+    }
+
+    // SEARCH
+    public E search(E value) {
+        return searchRec(root, value);
+    }
+
+    private E searchRec(TreeNode<E> node, E value) {
+        if (node == null) return null;
+
+        int cmp = value.compareTo(node.data);
+
+        if (cmp == 0) return node.data;
+
+        if (cmp < 0) return searchRec(node.left, value);
+
+        return searchRec(node.right, value);
+    }
+
+    // INORDER
+    public void inOrder() {
+        inOrderRec(root);
+    }
+
+    private void inOrderRec(TreeNode<E> node) {
+        if (node != null) {
+            inOrderRec(node.left);
+            System.out.println(node.data);
+            inOrderRec(node.right);
+        }
+    }
 }
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,1 +1,13 @@
 package src;
+public class TreeNode<E> {
+
+    E data;
+    TreeNode<E> left;
+    TreeNode<E> right;
+
+    public TreeNode(E data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
